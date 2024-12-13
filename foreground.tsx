5c93413df399cdb4fb34d631752859645caab346
Falk Sippach <falk@jug-da.de>
2024-12-13 02:37:35 +0100

    first punsch draft

---
+++ foreground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="foreground" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
+ <image source="foreground.png" width="1600" height="1600"/>
+</tileset>

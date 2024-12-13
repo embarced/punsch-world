5b9f00103989fd1564c1733aabf3e1cead80dba4
Falk Sippach <falk@jug-da.de>
2024-12-13 02:34:15 +0100

    first punsch draft

--- background.tsx
+++ background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="background" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
- <image source="../bilder/background.jpg" width="1600" height="1600"/>
+<tileset version="1.5" tiledversion="1.7.2" name="background" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
+ <image source="background.jpg" width="1600" height="1600"/>
 </tileset>

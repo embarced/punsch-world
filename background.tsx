0c119e49053fc4a5478d5de2e4cb60b28d443d6d
Falk Sippach <falk@jug-da.de>
2024-12-13 02:58:39 +0100

    first punsch draft

--- background.tsx
+++ background.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="background" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
  <image source="background.jpg" width="1600" height="1600"/>
+ <tile id="1400">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
